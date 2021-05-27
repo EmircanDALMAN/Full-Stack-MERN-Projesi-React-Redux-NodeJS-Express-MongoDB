ae8be32babba38382a64b5d091fe4a49dff3c5b1
Emircan Dalman <emircandalman28@gmail.com>
2021-05-27 21:41:51 +0300

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {Provider} from "react-redux";
-import {applyMiddleware, compose, createStore} from "redux";
-import thunk from "redux-thunk";
-import reducers from "./reducers";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+import { reducers } from './reducers';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById("root"));
+const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
